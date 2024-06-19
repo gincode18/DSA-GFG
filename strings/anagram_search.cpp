d81260847a6e1ef7f5d56c78d4315a88092bfe06
gincode18 <vishalkamboj9211@gmail.com>
2024-06-19 23:36:40 +0530

    day_19 :)

--- strings/anagram_search.cpp
+++ strings/anagram_search.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
+#include <unordered_map>
+#define CHAR 256
+
 using namespace std;
 
-int anagram_search()
+bool issame(int p[], int t[])
 {
-    return 0;
+    for (int i = 0; i < CHAR; i++)
+    {
+        if (p[i] != t[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int anagram_search(string txt, string pat)
+{
+    int p[CHAR] = {0};
+    int t[CHAR] = {0};
+    for (int i = 0; i < pat.length(); i++)
+    {
+        p[(int)pat[i]] += 1;
+        t[(int)txt[i]] += 1;
+    }
+    for (int i = pat.length(); i <= txt.length(); i++)
+    {
+        if (issame(p, t))
+            return true;
+        t[(int)txt[i]] += 1;
+        t[(int)txt[i - pat.length()]] -= 1;
+    }
+
+    return false;
 }
 
 int main()
 {
+    string txt = "halvsi";
+    string pat = "vis";
+    bool res = anagram_search(txt, pat);
+    cout << res;
     return 0;
 }
