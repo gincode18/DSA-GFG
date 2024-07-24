f868934eae5e9a15c0d30c85b8f31a7ba208c784
gincode18 <vishalkamboj9211@gmail.com>
2024-07-25 00:26:32 +0530

    day_37 :)

--- graphs/distance_of_nearest_cell_having_1.cpp
+++ graphs/distance_of_nearest_cell_having_1.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <climits> // for INT_MAX
+
 #define MAX 500
 using namespace std;
 
 class Graph
 {
-
 public:
     vector<int> g[MAX];
     int n;
@@ -14,40 +15,35 @@ public:
 
     Graph(int a, int b)
     {
-        this->n = a;
         this->m = b;
-        g->resize(a);
+        this->n = a;
     }
 
     void create_graph()
     {
         int k = 1;
 
-        for (int i = 0; i <= n; i++)
+        for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j <= m; j++)
+            for (int j = 0; j < m; j++)
             {
                 // If last row, then add edge on right side.
-                if (i == n)
+                if (i == n-1)
                 {
                     // If not bottom right cell.
-                    if (j != m)
+                    if (j != m-1)
                     {
                         g[k].push_back(k + 1);
                         g[k + 1].push_back(k);
                     }
                 }
-
-                // If last column, then add edge toward
-                // down.
-                else if (j == m)
+                // If last column, then add edge downward.
+                else if (j == m-1)
                 {
                     g[k].push_back(k + m);
                     g[k + m].push_back(k);
                 }
-
-                // Else makes an edge in all four
-                // directions.
+                // Else makes an edge in all four directions.
                 else
                 {
                     g[k].push_back(k + 1);
@@ -63,14 +59,13 @@ public:
 
     void bfs(vector<bool> &visited, vector<int> &distance, queue<int> &q)
     {
-        while (q.empty() == false)
+        while (!q.empty())
         {
             int front = q.front();
             q.pop();
             for (int i = 0; i < g[front].size(); i++)
             {
-
-                if (visited[g[front][i]] == false)
+                if (!visited[g[front][i]])
                 {
                     distance[g[front][i]] = min(distance[g[front][i]], distance[front] + 1);
                     q.push(g[front][i]);
@@ -83,10 +78,8 @@ public:
     void print(vector<int> &distance)
     {
         int count = 0;
-
         for (int i = 0; i < n; i++)
         {
-
             for (int j = 0; j < m; j++)
             {
                 count++;
@@ -99,23 +92,27 @@ public:
 
 void distance_of_nearest_cell_having_1(vector<vector<int>> arr)
 {
-    vector<int> distance(arr.size() * arr[0].size(), INT_MAX);
-    vector<bool> visited(arr.size() * arr[0].size(), 0);
+    int rows = arr.size();
+    int cols = arr[0].size();
+    vector<int> distance((rows * cols) + 1, INT_MAX);
+    vector<bool> visited((rows * cols) + 1, false);
     queue<int> q;
 
-    Graph g(arr.size(), arr[0].size());
+    Graph g(rows, cols);
     g.create_graph();
 
     int k = 1;
-    for (int i = 0; i < arr.size(); i++)
+    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < arr[0].size(); j++)
+        for (int j = 0; j < cols; j++)
         {
             if (arr[i][j] == 1)
             {
                 q.push(k);
-                k++;
+                distance[k] = 0; // Set the distance to 0 for the cells with 1
+                visited[k] = true;
             }
+            k++;
         }
     }
     g.bfs(visited, distance, q);
